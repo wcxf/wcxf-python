e19106ecb1b5f7481ad6d1c94677058056a12e6a
David Straub <david.straub@tum.de>
2017-10-27 11:28:08 +0200

    Read all EFT and basis files from submodule

--- wcxf/__init__.py
+++ wcxf/__init__.py
@@ -1,2 +1,19 @@
 from .classes import *
-from . import translators
+from . import matchers
+
+# read all EFTs and bases from the wcxf-bases submodule
+
+import os
+import glob
+
+_root = os.path.abspath(os.path.dirname(__file__))
+all_efts = glob.glob(os.path.join(_root, 'bases', '*.eft.yml'))
+all_bases = glob.glob(os.path.join(_root, 'bases', '*.basis.yml'))
+
+for eft in all_efts:
+    with open(eft, 'r') as f:
+        EFT.load(f)
+
+for basis in all_bases:
+    with open(basis, 'r') as f:
+        Basis.load(f)
