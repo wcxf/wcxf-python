3f525c88ad6c776fe9f38672ba23f59437759157
David Straub <david.straub@tum.de>
2017-11-07 21:04:53 +0100

    Load parent bases before child bases

--- wcxf/__init__.py
+++ wcxf/__init__.py
@@ -9,11 +9,12 @@ import glob
 _root = os.path.abspath(os.path.dirname(__file__))
 all_efts = glob.glob(os.path.join(_root, 'bases', '*.eft.yml'))
 all_bases = glob.glob(os.path.join(_root, 'bases', '*.basis.yml'))
+child_bases = glob.glob(os.path.join(_root, 'bases', 'child', '*.basis.yml'))
 
 for eft in all_efts:
     with open(eft, 'r') as f:
         EFT.load(f)
 
-for basis in all_bases:
+for basis in all_bases + child_bases:
     with open(basis, 'r') as f:
         Basis.load(f)
