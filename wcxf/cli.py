5877c76a6ccebf0e90b45d9a774b4bb943a28958
David Straub <david.straub@tum.de>
2017-11-16 15:54:50 +0100

    Move YAML/JSON converter to new converters module

--- wcxf/cli.py
+++ wcxf/cli.py
@@ -1,25 +1,13 @@
 import argparse
 import wcxf
-import yaml
-import json
 import sys
 import logging
+import os
+import yaml
 
-def convert_json(stream_in, stream_out):
-    try:
-        return wcxf.classes._yaml_to_json(stream_in, stream_out, indent=2)
-    except yaml.YAMLError:
-        logging.error("Input file cannot be parsed as YAML.")
-        return 1
-
-def convert_yaml(stream_in, stream_out):
-    try:
-        return wcxf.classes._json_to_yaml(stream_in, stream_out, default_flow_style=False)
-    except json.decoder.JSONDecodeError:
-        logging.error("Input file cannot be parsed as JSON.")
-        return 1
 
 def convert():
+    from wcxf.converters.yamljson import convert_json, convert_yaml
     parser = argparse.ArgumentParser(description="""Command line script to convert WCxf files between YAML and JSON.""",
                                      formatter_class=argparse.RawTextHelpFormatter)
     parser.add_argument("FORMAT", help="Output format (should be yaml or json)", type=str)
