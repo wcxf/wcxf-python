b1b5f1e9cdc9be543954f6464c68aed3472a8b4a
David Straub <david.straub@tum.de>
2017-12-04 17:51:47 +0100

    JMS->EOS translator: add to init

--- wcxf/translators/__init__.py
+++ wcxf/translators/__init__.py
@@ -2,13 +2,21 @@ from . import smeftmass
 from . import wet
 import wcxf
 
+
 @wcxf.translator('SMEFT', 'Warsaw', 'Warsaw mass')
 def warsaw_to_warsawmass(C, parameters):
     return smeftmass.warsaw_to_warsawmass(C)
 
+
 @wcxf.translator('WET', 'JMS', 'flavio')
 def JMS_to_flavio(C, parameters):
     return wet.JMS_to_flavio(C, parameters)
+
+
+@wcxf.translator('WET', 'JMS', 'EOS')
+def JMS_to_EOS(C, parameters):
+    return wet.JMS_to_EOS(C, parameters)
+
 
 @wcxf.translator('WET', 'JMS', 'AFGV')
 def JMS_to_Bern(C, parameters):
