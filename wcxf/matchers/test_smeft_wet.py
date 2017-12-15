e9cadc7ee63612094c9eba5ea54248d700b0a85e
David Straub <david.straub@tum.de>
2017-12-15 09:40:15 +0100

    Additional unit tests for matchers

--- wcxf/matchers/test_smeft_wet.py
+++ wcxf/matchers/test_smeft_wet.py
@@ -18,29 +18,37 @@ for sector, wcs in basis.sectors.items():
              C_Warsaw_random[name] += 1j*1e-6*np.random.rand()
 v_Warsaw_random = wcxf.WC.dict2values(C_Warsaw_random)
 wc_Warsaw_random = wcxf.WC('SMEFT', 'Warsaw', scale=160,
-                          values=v_Warsaw_random)
+                           values=v_Warsaw_random)
+wc_Warsaw_minimal1 = wcxf.WC('SMEFT', 'Warsaw', scale=160,
+                             values={'G': 1.2})
+wc_Warsaw_minimal2 = wcxf.WC('SMEFT', 'Warsaw', scale=160,
+                             values={'ed_1123':  {'Re': 1.2}})
 
 class TestSMEFTWET(unittest.TestCase):
     def test_warsaw_jms(self):
-        wc = wc_Warsaw_random.match('WET', 'JMS')
-        self.assertEqual(wc.eft, 'WET')
-        self.assertEqual(wc.basis, 'JMS')
-        wc.validate()
+        for wcW in [wc_Warsaw_random, wc_Warsaw_minimal1, wc_Warsaw_minimal2]:
+            wc = wcW.match('WET', 'JMS')
+            self.assertEqual(wc.eft, 'WET')
+            self.assertEqual(wc.basis, 'JMS')
+            wc.validate()
 
     def test_warsaw_flavio(self):
-        wc = wc_Warsaw_random.match('WET', 'flavio')
-        self.assertEqual(wc.eft, 'WET')
-        self.assertEqual(wc.basis, 'flavio')
-        wc.validate()
+        for wcW in [wc_Warsaw_random, wc_Warsaw_minimal1, wc_Warsaw_minimal2]:
+            wc = wcW.match('WET', 'flavio')
+            self.assertEqual(wc.eft, 'WET')
+            self.assertEqual(wc.basis, 'flavio')
+            wc.validate()
 
     def test_warsaw_EOS(self):
-        wc = wc_Warsaw_random.match('WET', 'EOS')
-        self.assertEqual(wc.eft, 'WET')
-        self.assertEqual(wc.basis, 'EOS')
-        wc.validate()
+        for wcW in [wc_Warsaw_random, wc_Warsaw_minimal1, wc_Warsaw_minimal2]:
+            wc = wcW.match('WET', 'EOS')
+            self.assertEqual(wc.eft, 'WET')
+            self.assertEqual(wc.basis, 'EOS')
+            wc.validate()
 
     def test_warsaw_Bern(self):
-        wc = wc_Warsaw_random.match('WET', 'Bern')
-        self.assertEqual(wc.eft, 'WET')
-        self.assertEqual(wc.basis, 'Bern')
-        wc.validate()
+        for wcW in [wc_Warsaw_random, wc_Warsaw_minimal1, wc_Warsaw_minimal2]:
+            wc = wcW.match('WET', 'Bern')
+            self.assertEqual(wc.eft, 'WET')
+            self.assertEqual(wc.basis, 'Bern')
+            wc.validate()
