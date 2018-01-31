4b00ff52ce766535c5202001f13d138ca5ce7492
David Straub <david.straub@tum.de>
2018-01-31 15:34:51 +0100

    Version -> 1.2.2

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup, find_packages
 
 setup(name='wcxf',
-      version='1.2.1',
+      version='1.2.2',
       author='David M. Straub, Jason Aebischer',
       author_email='david.straub@tum.de, jason.aebischer@tum.de',
       license='MIT',
