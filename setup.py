63b112ec8079db53f1410eb57a38f08fb7ca8844
David Straub <david.straub@tum.de>
2018-08-20 12:00:10 +0200

    [setup] Version -> 1.4.3, metadata added

--- setup.py
+++ setup.py
@@ -1,10 +1,16 @@
 from setuptools import setup, find_packages
 
+with open('README.md') as f:
+    LONG_DESCRIPTION = f.read()
+
 setup(name='wcxf',
-      version='1.4.2',
+      version='1.4.3',
       author='David M. Straub, Jason Aebischer',
       author_email='david.straub@tum.de, jason.aebischer@tum.de',
       license='MIT',
+      url='https://wcxf.github.io',
+      description='Python API and command line interface for the Wilson Coefficient exchange format',
+      long_description=LONG_DESCRIPTION,
       packages=find_packages(),
       package_data={
         'wcxf': ['data/*.yml',
