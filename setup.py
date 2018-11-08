402343bcb28215243b58805cc351661bb12de931
David Straub <david.straub@tum.de>
2018-11-08 11:31:08 +0100

    Version -> 1.4.7

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ with open('README.md') as f:
     LONG_DESCRIPTION = f.read()
 
 setup(name='wcxf',
-      version='1.4.6',
+      version='1.4.7',
       author='David M. Straub, Jason Aebischer',
       author_email='david.straub@tum.de, jason.aebischer@tum.de',
       license='MIT',
