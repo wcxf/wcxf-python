15bf66bba2f19613c29a23a73cd3da34b7c401e4
David Straub <david.straub@tum.de>
2019-07-05 11:05:59 +0200

    Correct symmetry factors in wcxf2smeftsim and add unit test (#22)

--- wcxf/converters/test_smeftsim.py
+++ wcxf/converters/test_smeftsim.py
@@ -4,6 +4,7 @@ from tempfile import mkdtemp
 import subprocess
 import pylha
 from shutil import rmtree
+import wcxf
 
 
 my_path = os.path.dirname(os.path.realpath(__file__))
@@ -25,8 +26,30 @@ class TestSMEFTsim(unittest.TestCase):
             self.assertTrue(os.path.isfile, outf)
             # check if can be imported as LHA
             with open(outf, 'r') as f:
-                out = pylha.load(f)
-            # check string is not empty
-            self.assertTrue(out)
+                card = pylha.load(f)
+            # check dict is not empty
+            self.assertTrue(card)
             # remove tmpdir
             rmtree(tmpdir)
+
+    def test_symm_fac(self):
+        wc = wcxf.WC('SMEFT', 'Warsaw mass', 120,
+                     {'ll_1111': 1e-6, 'll_1221': 4e-6})
+        tmpdir = mkdtemp()
+        with open(os.path.join(tmpdir, 'my_wcxf.yaml'), 'w') as f:
+            wc.dump(f, fmt='yaml')
+        res = subprocess.run(['wcxf2smeftsim', 'my_wcxf.yaml'],
+                             cwd=tmpdir)
+        # check if file is present
+        outf = os.path.join(tmpdir, 'wcxf2smeftsim_param_card.dat')
+        self.assertTrue(os.path.isfile, outf)
+        with open(outf, 'r') as f:
+            card = pylha.load(f)
+        self.assertEqual(dict(card['BLOCK']['FRBlock']['values'])[691],
+                         1e6 * 4e-6 / 2,  # symmetry factor of 1 / 2!
+                         msg="Wrong value for ll_1221")
+        self.assertEqual(dict(card['BLOCK']['FRBlock']['values'])[689],
+                         1e6 * 1e-6,  # no symmetry factor!
+                         msg="Wrong value for ll_1111")
+        # remove tmpdir
+        # rmtree(tmpdir)
